7c2a47a462c0daeab1b9621d61b651d3fbdf54bb
ProjectBYH <wnrhd45@gmail.com>
2022-10-13 16:50:45 +0900

    폰트수정

--- client/src/compenents/detailmain/DetailMain.js
+++ client/src/compenents/detailmain/DetailMain.js
@@ -29,10 +29,11 @@ export const TitleContainer = styled.div`
   width: 70%;
   margin-left: 10rem;
   & > div > h1 {
-    font-family: 'MaplestoryOTFBold'
+    font-family: 'MaplestoryOTFBold';
     font-size: x-large;
   }
   & > div > h2 {
+    font-family: 'MaplestoryOTFBold';
     font-size: large;
   }
 `;
@@ -61,7 +62,18 @@ export const AirCoverContainer = styled.div`
   }
 
   & > nav > h1 {
+    font-family: 'MaplestoryOTFBold';
     color: red;
+  }
+
+  & > h1 {
+    font-family: 'MaplestoryOTFBold';
+  }
+  & > h2 {
+    font-family: 'MaplestoryOTFBold';
+  }
+  & > h3 {
+    font-family: 'MaplestoryOTFBold';
   }
 `;
 
@@ -77,6 +89,9 @@ export const TagContainer = styled.div`
     border-style: solid;
     outline: 0;
     border-radius: 10% / 50%;
+  }
+  & > h2 {
+    font-family: 'MaplestoryOTFBold';
   }
 `;
 
